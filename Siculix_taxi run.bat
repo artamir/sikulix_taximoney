1b7bd556b1215325c82abcefcd4234ad924a6859
artamir <artamir@mail.ru>
2023-03-23 13:11:10 +0300

    .

---
+++ Siculix_taxi run.bat
@@ -0,0 +1,4 @@
+rem E:\vanessa\sikulix\runsikulix -r "E:\Siculix_MAIB.sikuli" --args -sd 2022-09-21 -ed 2022-09-30>responce.txt
+rem E:\vanessa\sikulix\runsikulix -r "E:\Siculix_MAIB.sikuli" --args -d 2022-09-30>responce.txt
+call c:\sikulix\runsikulix -r "c:\Sikulix_scripts\git_sikulix_taximoney\sikulix_taximoney\Sikulix_TaxiMoneyV2.sikuli" --args -sd 2022-11-20 -ed 2022-11-30>responce.txt
+pause
---
+++ taxi_test.sikuli/taxi_test.py
@@ -0,0 +1,19 @@
+type(r"f",KeyModifier.CTRL)
+paste(unicd("работа"))
+type(Key.ENTER)
+
+Pattern("1679491809501.png").targetOffset(-177,57) 
+
+"rabota blue.png"
+
+"1679495102267.png"
+
+"bonus blue.png"
+
+"1679495289633.png"
+
+"1679510979256.png"
+
+Pattern("1679511014150.png").similar(0.64)
+
+
